50b18e979e712b8e93179fec4c9fb5d73d88f883
Jacob Carver <jacxbcarver@gmail.com>
2019-07-28 14:24:26 -0600

    initial webpack setup w/ jquery and sass

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import $ from 'jquery';
+import './main.scss';
+
+$(document).ready(function () {
+	console.log('Ready')
+});
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+	mode: 'development',
+	entry: './src/index.js',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	module: {
+		rules: [
+			{
+				test: /\.scss$/,
+				use: [
+					'style-loader',
+					'css-loader',
+					'sass-loader'
+				]
+			},
+			{
+				test: /\.(png|svg|jpg|gif)$/,
+				use: [
+					'file-loader'
+				]
+			}
+		]
+	}
+};
